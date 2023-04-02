1071b225aff53c02153b7b5d0ad992285f85c4be
FHorvat9 <121050381+FHorvat9@users.noreply.github.com>
2023-04-02 18:13:07 +0200

    OibGenerator promjenjen da vraca oib

--- Java/Netbeans/oibGenerator/src/main/java/edunova/OibGenerator.java
+++ Java/Netbeans/oibGenerator/src/main/java/edunova/OibGenerator.java
@@ -8,20 +8,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class OibGenerator {
 
-    public OibGenerator() {
-       
-        generirajOib(10000);
-       
-        
+    public static void main(String[] args) {
+        System.out.println(generirajOib());
     }
 
-    public static void generirajOib(int kolicina) {
-        for(int x=0;x<kolicina;x++){
+    public static String generirajOib() {
+
         StringBuilder oib = new StringBuilder();
         oib.append(slucajniBroj(10));
         int znamenka = Integer.parseInt(String.valueOf(oib.charAt(0))) + 10;
         int kontrolnaZnamenka = 0;
-        for (int i = 0, j = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
 
             if (znamenka % 10 == 0) {
                 znamenka = 10;
@@ -34,24 +31,20 @@ public class OibGenerator {
                 break;
             }
             znamenka += Integer.parseInt(String.valueOf(oib.charAt(i + 1)));
-            
+
         }
-        
+
         if (11 - znamenka != 10) {
-            
+
             kontrolnaZnamenka = 11 - znamenka;
         }
-       
-        System.out.println(x+1+". oib je: " + oib.append(kontrolnaZnamenka));
-        }
-    }
-    public static long slucajniBroj(int brojznamenki) {
-    long min = (long) Math.pow(10, brojznamenki - 1);
-    return ThreadLocalRandom.current().nextLong(min, min * 10);
-}
-
-    public static void main(String[] args) {
-        new OibGenerator();
+        oib.append(kontrolnaZnamenka);
+        
+        return oib.toString();
     }
 
+    private static long slucajniBroj(int brojznamenki) {
+        long min = (long) Math.pow(10, brojznamenki - 1);
+        return ThreadLocalRandom.current().nextLong(min, min * 10);
+    }
 }
